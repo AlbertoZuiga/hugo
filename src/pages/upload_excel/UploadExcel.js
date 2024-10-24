a501843a91b365ae0b538cd56064e3e8380906e3
Alberto Zúñiga <azuiga@miuandes.cl>
2024-10-24 02:28:16 -0300

    improoved styles in upload excel

--- src/pages/upload_excel/UploadExcel.js
+++ src/pages/upload_excel/UploadExcel.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { uploadExcelApi } from '../../api/uploadExcelApi'; // Importa el servicio
-import "./UploadExcel.css"; // Asegúrate de tener este archivo CSS
+import './UploadExcel.css'; // Asegúrate de tener este archivo CSS
 
 const UploadExcel = () => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
   const [successMessage, setSuccessMessage] = useState('');
+  const [loading, setLoading] = useState(false); // Nuevo estado para cargar
 
   // Función para manejar el cambio de archivo
   const handleFileChange = (event) => {
@@ -24,6 +25,7 @@ const UploadExcel = () => {
     }
 
     const token = localStorage.getItem('authToken');
+    setLoading(true); // Comienza la carga
 
     try {
       const result = await uploadExcelApi(file, token); // Llama a la función del servicio
@@ -33,18 +35,28 @@ const UploadExcel = () => {
     } catch (error) {
       setError(`Error: ${error.detail || error}`); // Maneja el error de forma adecuada
       console.error('Error al subir el archivo:', error);
+    } finally {
+      setLoading(false); // Finaliza la carga
     }
   };
 
   return (
-    <div>
-      <h2>Subir archivo Excel</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="file" accept=".xlsx, .xls" onChange={handleFileChange} />
-        <button type="submit">Subir</button>
+    <div className="upload-excel-container">
+      <h2 className="upload-excel-title">Subir archivo Excel</h2>
+      <form className="upload-excel-form" onSubmit={handleSubmit}>
+        <input 
+          type="file" 
+          accept=".xlsx, .xls" 
+          onChange={handleFileChange} 
+          className="upload-excel-input"
+        />
+        <button type="submit" className={`upload-excel-button ${loading ? 'loading' : ''}`} disabled={loading}>
+          {loading ? 'Cargando...' : 'Subir'}
+        </button>
       </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+      {error && <p className="upload-excel-error">{error}</p>}
+      {successMessage && <p className="upload-excel-success">{successMessage}</p>}
+      {loading && <div className="spinner"></div>} {/* Spinner de carga */}
     </div>
   );
 };
